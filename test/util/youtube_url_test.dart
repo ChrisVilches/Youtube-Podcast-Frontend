e9ce9fdc9d6478cae7c3f387251a8aa762ca1e67
Chris Vilches <felovilches@gmail.com>
2023-01-24 08:58:32 +0900

    format file

--- test/util/youtube_url_test.dart
+++ test/util/youtube_url_test.dart
@@ -3,7 +3,13 @@ import 'package:youtube_podcast/src/util/youtube_url.dart';
 
 void main() {
   test(vQueryParam, () {
-    expect(vQueryParam('http://somehost.com/?w=34343&v=qwertyuq1234'), 'qwertyuq1234');
-    expect(vQueryParam('https://www.youtube.com/?v=ABCDEFGH&someflag'), 'ABCDEFGH');
+    expect(
+      vQueryParam('http://somehost.com/?w=34343&v=qwertyuq1234'),
+      'qwertyuq1234',
+    );
+    expect(
+      vQueryParam('https://www.youtube.com/?v=ABCDEFGH&someflag'),
+      'ABCDEFGH',
+    );
   });
 }
