36e3ea8bd48f2370fcc4fe635ac476a3e716e998
Chris Vilches <felovilches@gmail.com>
2023-01-31 00:17:21 +0900

    splash screen, themes, fonts

---
+++ lib/src/themes/light_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme = ThemeData(
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromRGBO(0xaa, 0x01, 0x01, 1),
+  ),
+  fontFamily: 'WorkSans',
+  popupMenuTheme: const PopupMenuThemeData(
+    color: Color.fromRGBO(0xaa, 0x00, 0x00, 0.8),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+    ),
+  ),
+);
